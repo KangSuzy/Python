3f273dc8fcfda9c094624aa6665b2955e4486ebd
KangSuzy <astnwl321@gmail.com>
2019-03-28 01:43:38 +0900

    review python

---
+++ variables -data_type.py
@@ -0,0 +1,29 @@
+"""print("Hello World")
+print("Mary\'s cosmatics")
+print("신씨가 소리질렀다. \"도둑이야\".")
+print("C:\\Windows")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("오늘은", "일요일")
+print("naver","kakao","sk","samsung", sep=";")
+print("first", end=' ');print("second", end=' ')
+str = "dk2jd923i1jdk2jd93jfd92jd918943jfd8923"
+print(len(str))"""
+
+s = "hello! "
+t = "python"
+print(s+t)
+
+print("-"*80)
+
+t1 = 'python '
+t2 = 'java '
+print((t1+t2)*4)
+
+a = "132"
+print(type(a))
+
+num_str = "720"
+print(int(num_str))
+
+num = 100
+print(str(num))
